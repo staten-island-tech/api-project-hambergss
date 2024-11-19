68eca51553a75d3dbc9c4e39dd1a2e4dd2b30ad7
hambergss <144480774+hambergss@users.noreply.github.com>
2024-11-19 14:00:27 -0500

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,1 +1,26 @@
 import "./style.css";
+
+async function getData() {
+  try {
+    //returns a promise
+    const response = await fetch("https://genshin.jmp.blue/");
+    //guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      //convert promise to json
+      const data = await response.json();
+      console.log(data.data);
+      //this is unique to THIS API
+      data.data.forEach((character) =>
+        document
+          .querySelector("div")
+          .insertAdjacentHTML("afterbegin", `<h1>${agent.displayName}</h1>`)
+      );
+    }
+  } catch (error) {
+    alert("hey I could not find that character for you");
+  }
+};
+
+getData();
