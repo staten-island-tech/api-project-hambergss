f019529f4154ed6dbef6361180853f024ae69a4d
hambergss <144480774+hambergss@users.noreply.github.com>
2024-11-27 13:04:44 -0500

    Update main.js

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,26 +1,36 @@
 import "../css/style.css";
 
 async function getData() {
-  try {
-    //returns a promise
-    const response = await fetch("https://genshin.jmp.blue/characters");
-    //guard clause
-    if (response.status != 200) {
-      throw new Error(response);
-    } else {
-      //convert promise to json
-      const data = await response.json();
-      console.log(data.data);
-      //this is unique to THIS API
-      data.data.forEach((character) =>
-        document
-          .querySelector("div")
-          .insertAdjacentHTML("afterbegin", `<h2>${character.displayName}</h2>`)
-      );
-    }
-  } catch (error) {
-    alert("hey I could not find that character for you");
-  }
-};
+  //returns a promise
+  const response = await fetch("https://genshin.jmp.blue/characters");
+  console.log(response.status);
 
+  const data = await response.json();
+  console.log(data.data);
+  //this is unique to THIS API
+
+  presentCharacters(data);
+}
 getData();
+
+function presentCharacters(data) {
+  const characterListContainer = document.querySelector("character-cont");
+  characterListContainer.innerHTML = "";
+
+  data.data.forEach((character) => {
+    const nameText = character.name
+      .map((name) => `<p>${name.name}: ${name.scaling}</p>`)
+      .join("");
+
+    const attackText = character.attack
+      .map((attack) => `<p>${attack.name}: ${attack.amount}</p>`)
+      .join("");
+
+    const defenseText = character.defense
+      .map((defense) => `<p>${defense.name}: ${defense.amount}</p>`)
+      .join("");
+    
+    const characterHTML = `
+    `
+  });
+}
